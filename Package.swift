a3487c1bd76dda759ee17d0814ee0d6d30b14dec
Ahmed Talaat <ahmedtal@banquemisr.com>
2024-01-09 19:22:03 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FloatingButtonView",
-            dependencies: ["RxSwift", "RxCocoa"],
+            dependencies: ["RxSwift",
+                           .product(name: "RxCocoa", package: "RxSwift")],
             path: "Sources"),
         .testTarget(
             name: "FloatingButtonViewTests",
